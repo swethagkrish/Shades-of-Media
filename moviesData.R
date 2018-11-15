21b40c70f7a4e0ee76b1e11dc19c6e5def6619d4
Swetha <swethg@uw.edu>
2018-11-15 14:49:23 -0800

    Changed the dataframe slighly for the movies

--- moviesData.R
+++ moviesData.R
@@ -1,3 +1,4 @@
 library(dplyr)
 data <- read.csv("movies.csv", stringsAsFactors = FALSE)
-movies <- data %>% select(title,release_date, vote_average, vote_count,revenue, popularity, budget, imdb_id, id)
+movies <- data %>% select(title,release_date, vote_average, vote_count,revenue, budget, imdb_id)
+

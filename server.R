dbc04b2d3894094344cd81189b755c4e003ee67c
Flip3 <43688594+Flip3@users.noreply.github.com>
2018-12-04 14:29:32 -0800

    shiny app for plotting revenue

--- server.R
+++ server.R
@@ -7,33 +7,43 @@ dataF <- read.csv(file = "../mainData.csv" , sep = ",", stringsAsFactors = TRUE)
 dataF <- as.data.frame(dataF)
 
 
-#dataF <- dataF %>% 
-#  filter(revenue != 0)
-#dataF <- na.omit(dataF)
 
-predomF <- c()
-predomM <- c()
+predomFRev <- c()
+predomMRev <- c()
+predomFYear <- c()
+predomMYear <- c()
 
-# Sort out the imdb_ids of movies that are predominantly female and male
+
+# Sort out the imdb_ids of movies that are predominantly female and male in loop
 for (i in levels(dataF$imdb_id)) {
   data = subset(dataF, dataF$imdb_id == i)
   numFemales = count(data[data$gender == "f",])
   numMales = count(data[data$gender == "m",])
+  rev <- c(data[1,"revenue"])
+  year <- c(data[1,"year"])
   if (numFemales > numMales) {
-    predomF <- c(predomF, data[1,"revenue"])
+    predomFRev <- c(predomFRev, rev)
+    predomFYear <- c(predomFYear, year)
   } else {
-    predomM <- c(predomM, data[1,"revenue"])
+    predomMRev <- c(predomMRev, rev)
+    predomMYear <- c(predomMYear, year)
   }
 }
 
 
 
 shinyServer(function(input, output) {
-   
   output$distPlot <- renderPlot({
-    plot(density(predomM))
-    lines(density(predomF))
-
+    dateRange <- reactive({return(input$daterange)})
+    #Convert string date value into interger to use for plotting the years
+    startYear <- strtoi(substring(dateRange()[1], 1, 4))
+    endYear <- strtoi(substring(dateRange()[2], 1, 4))
+    #plot made, male data first
+    plot(predomMYear, predomMRev / 100000000, xlim = c(startYear, endYear), pch = 16, col = "lightblue1", xlab = ("Year"), ylab = ("Revenue (100M USD)"))
+    #adding female data points
+    points(predomFYear, predomFRev / 100000000, xlim = c(startYear, endYear), pch = 16, col = "lightpink")
+    
+    
   })
   
 })
--- ui.R
+++ ui.R
@@ -5,18 +5,18 @@ library(ggplot2)
 
 
 shinyUI(fluidPage(
-  titlePanel("IMDB Movie Revenue Based on Gender"),
+  titlePanel("Gender Comparison: IMDB Movie Revenue VS. Time"),
   
-#  sidebarLayout(
-#   sidebarPanel(
-#      
-#      selectInput("bins",
-#                  "Date Range: ",
-#                  c("a","b"))
-#    ),
+  sidebarLayout(
+   sidebarPanel(
+      dateRangeInput("daterange", "Year Range", format = "yyyy",
+                     min = "1931-01-01", max = "2015-12-31", 
+                     start = "1931-01-01", end = "2015-12-31",
+                     startview = "decade")),
     
     mainPanel(
       plotOutput("distPlot")
     )
   )
+)
 )

37954d7d7ac8bc0cf0241e367bba30c38937e1d8
swethagkrish <43593010+swethagkrish@users.noreply.github.com>
2018-12-04 18:35:51 -0800

    Update server.R

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -8,6 +8,33 @@ library(stringr)
 charData <- read.csv("charData.csv") #For the app need to switch data set and put ../
 fillColors <- c("f" = "lightpink", "m" = "lightblue1")
 
+dataF <- read.csv(file = "../mainData.csv" , sep = ",", stringsAsFactors = TRUE)
+dataF <- as.data.frame(dataF)
+
+
+
+predomFRev <- c()
+predomMRev <- c()
+predomFYear <- c()
+predomMYear <- c()
+
+
+# Sort out the imdb_ids of movies that are predominantly female and male in loop
+for (i in levels(dataF$imdb_id)) {
+  data = subset(dataF, dataF$imdb_id == i)
+  numFemales = count(data[data$gender == "f",])
+  numMales = count(data[data$gender == "m",])
+  rev <- c(data[1,"revenue"])
+  year <- c(data[1,"year"])
+  if (numFemales > numMales) {
+    predomFRev <- c(predomFRev, rev)
+    predomFYear <- c(predomFYear, year)
+  } else {
+    predomMRev <- c(predomMRev, rev)
+    predomMYear <- c(predomMYear, year)
+  }
+}
+
 my_server <- function(input, output) {
   data <- read.csv("yearly.csv", stringsAsFactors = FALSE)
   output$plot <- renderPlotly({
@@ -128,7 +155,19 @@ my_server <- function(input, output) {
      totalWords <- sum(charData$words)
      paste0("In ", input$movie, "there are ", totalChar, "characters and ", totalWords, " spoken. There are ",
             femaleChar, "female characters that say a total of ", fWordsTotal, " words. ")
-     
    })
+  
+  output$distPlot <- renderPlot({
+    dateRange <- reactive({return(input$daterange)})
+    #Convert string date value into interger to use for plotting the years
+    startYear <- strtoi(substring(dateRange()[1], 1, 4))
+    endYear <- strtoi(substring(dateRange()[2], 1, 4))
+    #plot made, male data first
+    plot(predomMYear, predomMRev / 100000000, xlim = c(startYear, endYear), pch = 16, col = "lightblue1", xlab = ("Year"), ylab = ("Revenue (100M USD)"))
+    #adding female data points
+    points(predomFYear, predomFRev / 100000000, xlim = c(startYear, endYear), pch = 16, col = "lightpink")
+    
+    
+  })
 }
 shinyServer(my_server)

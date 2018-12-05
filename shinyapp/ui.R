c6eccb0a57f5a9caea8d55adcb8bac0d40bdc796
swethagkrish <43593010+swethagkrish@users.noreply.github.com>
2018-12-04 18:53:47 -0800

    Update ui.R

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -29,7 +29,40 @@ my_ui <- fluidPage(theme = shinytheme("cyborg"),
     mainPanel(
       plotlyOutput("charwords")
     )
-  ), br(), dfdsfdf, br(), 
+  ), br(),   titlePanel(h1(strong("IMDB Movie Ratings Over Time")), 
+             h2(strong("comparing ratings of movies with a male lead 
+                       versus the ratings of movies with a female lead"))),
+  
+  p(em("IMDB ratings are based upon the general population's vote.  This visualization provides 
+       a comparison of people's ratings of movies with a strong male lead versus those with a female
+       protagonist.")),
+  
+  sidebarLayout(
+    sidebarPanel(
+      dateRangeInput("date", "Date Range:", start = start_date, end = end_date)
+    ), 
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Comparison",
+                 p(em("This graph contains", num_movies, "movies from", "different years.  It ranges from
+                      ", start_date_words, "to ", end_date_words, ".  It compares the ratings for both
+                      movies with male and female leads on the same graph.")),
+                 plotOutput("mygraph")),
+        
+        tabPanel("Male",
+                 p(em("This graph contains", num_movies_male, "movies from", "different years.  It ranges from
+                      ", start_date_male_words, "to ", end_date_male_words, ".")),
+                 plotOutput("mygraphmale")),
+        
+        tabPanel("Female",
+                 p(em("This graph contains", num_movies_female, "movies from", "different years.  It ranges from
+                      ", start_date_female_words, "to ", end_date_female_words, ".")),
+                 plotOutput("mygraphfemale"))
+      )
+  )
+  
+), br(), 
                    
    titlePanel("Gender Comparison: IMDB Movie Revenue VS. Time"),
   

19efaaf78f45320b16418add0ea2a1f3f8cb323e
Brianna McCleerey <bmccleerey100@gmail.com>
2018-12-05 11:32:25 -0800

    Puts charts into tabs

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -8,33 +8,41 @@ library(shinythemes)
 source("../ratings_data.R")
 
 my_ui <- fluidPage(theme = shinytheme("superhero"), 
-
-                  titlePanel("Gender Representation in Movies"), 
-                  h4("INFORMATION ABOUT DATA AND OUR PROJECT"), 
-                  mainPanel(plotlyOutput("plot", height  = 500, width = 750), textOutput("summary")),
-                  br(), 
-                  sidebarPanel(
-                        sliderInput("Year", h2("Select a Year"),
-      min = 1971, max = 2015, value = 1971, sep = "",
-      step = 1, animate = animationOptions(interval = 1000, loop = FALSE)
-    ),
-    radioButtons(
-      "Rankings", h2("Select to Rank by IMDB Rating or Revenue"),
-      c("IMDB", "Revenue")
-    ), h3("Due to the high volume of data for certain years, we have decided to select only 15 films.  
+tabsetPanel(
+  tabPanel("Home",
+                titlePanel("Gender Representation in Movies"), 
+                h4("INFORMATION ABOUT DATA AND OUR PROJECT")
+  ),
+  tabPanel("Chart1",
+           sidebarLayout(
+             sidebarPanel(
+               selectizeInput('movie', "Search for a movie:", choices = NULL, multiple = FALSE, uiOutput("movieSearch"))
+             ),
+             mainPanel(
+               plotlyOutput("charwords")
+             )
+           ), br()
+      ),
+    tabPanel("Chart2",
+             mainPanel(plotlyOutput("plot", height  = 500, width = 750), textOutput("summary")),
+             br(), 
+             sidebarPanel(
+               sliderInput("Year", h2("Select a Year"),
+                           min = 1971, max = 2015, value = 1971, sep = "",
+                           step = 1, animate = animationOptions(interval = 1000, loop = FALSE)
+               ),
+               radioButtons(
+                 "Rankings", h2("Select to Rank by IMDB Rating or Revenue"),
+                 c("IMDB", "Revenue")
+               ), h3("Due to the high volume of data for certain years, we have decided to select only 15 films.  
           Select the IMDB button to rank films by IMDB ratings or Select Revenue to rank by the movie's 
-          gross.Select a year with the slider or click on the play 
-          button to see a time lapse of gender representation through the years."),
-          h3("Note: Visualization starts at 1971 instead of 1931, due to lack of data between 1931-1971")), 
-          br(), 
-          sidebarLayout(
-     sidebarPanel(
-       selectizeInput('movie', "Search for a movie:", choices = NULL, multiple = FALSE, uiOutput("movieSearch"))
-     ),
-     mainPanel(
-       plotlyOutput("charwords")
-     )
-   ), br(),  
+                     gross.Select a year with the slider or click on the play 
+                     button to see a time lapse of gender representation through the years."),
+               h3("Note: Visualization starts at 1971 instead of 1931, due to lack of data between 1931-1971")), 
+             br()
+             
+    ),
+   tabPanel("Chart3",
             titlePanel(h1(strong("IMDB Movie Ratings Over Time")), 
              h2(strong("comparing ratings of movies with a male lead 
                        versus the ratings of movies with a female lead"))),
@@ -69,7 +77,9 @@ my_ui <- fluidPage(theme = shinytheme("superhero"),
       )
   )
   
-), br(), 
+), br()
+),
+tabPanel("chart4",
                    
    titlePanel("Gender Comparison: IMDB Movie Revenue VS. Time"),
   
@@ -84,5 +94,5 @@ my_ui <- fluidPage(theme = shinytheme("superhero"),
       plotOutput("distPlot")
     )
   )                 
-  )
+  )))
 shinyUI(my_ui) 

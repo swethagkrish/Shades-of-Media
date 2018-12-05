213ff1827621ae1d8d3ccfcfc67d2e88888a3b98
Brianna McCleerey <bmccleerey100@gmail.com>
2018-12-04 21:27:45 -0800

    renamed file

--- finalproject.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-
-mainData <- read.csv("mainData.csv", stringsAsFactors = FALSE)
-
-charData <- select(mainData, title, imdb_character_name, words, gender)
-write.csv(charData, file = "charData.csv")
-
-testData <- filter(charData, title == "Toy Story")
-write.csv(testData, file = "charTestData.csv")

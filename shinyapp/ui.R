aed29b9f152528e24fb1af931f802a39557a0552
swethagkrish <43593010+swethagkrish@users.noreply.github.com>
2018-12-04 18:32:43 -0800

    Update ui.R

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -29,7 +29,20 @@ my_ui <- fluidPage(theme = shinytheme("cyborg"),
     mainPanel(
       plotlyOutput("charwords")
     )
-  ), 
+  ), br(), dfdsfdf, br(), 
                    
+   titlePanel("Gender Comparison: IMDB Movie Revenue VS. Time"),
+  
+  sidebarLayout(
+   sidebarPanel(
+      dateRangeInput("daterange", "Year Range", format = "yyyy",
+                     min = "1931-01-01", max = "2015-12-31", 
+                     start = "1931-01-01", end = "2015-12-31",
+                     startview = "decade")),
+    
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )                 
   )
 shinyUI(my_ui)
